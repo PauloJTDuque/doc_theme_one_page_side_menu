1c1a920656df9a82706c9cdbc409a859873bbd78
Paulo Duque <paulo.duque@terra.com.br>
2022-12-30 11:08:10 -0300

    Doc_Theme 3012

---
+++ wp-content/themes/doc-theme/page.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The template for displaying all single page
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Doc_Theme_One_Page_Side_Bar
+ * @subpackage Doc_Theme_One_Page_Side_Bar
+ * @since Doc_Theme_One_Page_Side_Bar 1.0
+ */
+
+get_header();
+
+
+
+
+
+
+
+get_footer();
---
+++ wp-content/themes/doc-theme/single.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Doc_Theme_One_Page_Side_Bar
+ * @subpackage Doc_Theme_One_Page_Side_Bar
+ * @since Doc_Theme_One_Page_Side_Bar 1.0
+ */
+
+get_header();
+
+
+
+
+
+
+
+
+get_footer();
